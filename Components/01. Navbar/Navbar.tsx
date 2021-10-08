1d353a2def55f16e0002dc00e695512f64d1e028
Seiji Matsumoto <seijim27@gmail.com>
2021-10-08 17:58:26 -0400

    Created top half of navbar

--- Components/01. Navbar/Navbar.tsx
+++ Components/01. Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "../../styles/01.Navbar/Navbar.module.css";
 
 const Navbar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.topNav}>
+        <div className={styles.topText}>
+          Get expert guidance and resources to confidently migrate your
+          workloads to DigitalOcean {">"}
+        </div>
+        <div className={styles.topLinks}>
+          <div className={styles.topLink}>We{"'"}re Hiring</div>
+          <div className={styles.topLink}>Blog</div>
+          <div className={styles.topLink}>Docs</div>
+          <div className={styles.topLink}>Get Support</div>
+          <div className={styles.topLink}>Sales</div>
+        </div>
+      </div>
+      <div className={styles.bottomNav}>Bottom</div>
+    </div>
+  );
 };
 
 export default Navbar;
