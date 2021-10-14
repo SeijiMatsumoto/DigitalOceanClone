9cc0c7d89a2b5f93ef7d44b0b39762fbec5b57a0
Seiji Matsumoto <seijim27@gmail.com>
2021-10-14 12:25:56 -0400

    Added dropdown for my links and changed styling

--- Components/01. Navbar/Navbar.tsx
+++ Components/01. Navbar/Navbar.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/01. Navbar/Navbar.module.css";
 import TopNav from "./TopNav";
 import BottomNav from "./BottomNav";
 import MobileNav from "./MobileNav";
 
 const Navbar: React.FC = () => {
+  // const [showSeiji, setShowSeiji] = useState<boolean>(false);
+
   return (
     <div className={styles.container}>
       <div className={styles.navComputer}>
         <TopNav />
         <BottomNav />
+        {/* {showSeiji ? <div className={styles.dropDown}>hello</div> : null} */}
+
       </div>
       <MobileNav />
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="description"
+            content="DigitalOcean - The developer cloud (Clone created by Seiji Matsumoto)"
+          />
+          <meta
+            name="image"
+            property="og:image"
+            content="https://images.prismic.io/www-static/0026a57b93abe5b04413765253903472dab58e11_general-droplets_blog-v4_twitter---facebook.png?auto=compress,format"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
