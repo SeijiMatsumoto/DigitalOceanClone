f9a16e96cbe834cbbaffb80e07b9fcb998972b65
SeijiMatsumoto <seijim27@gmail.com>
2022-09-06 13:50:29 -0400

    Remove get started

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
       <Scale />
       <Community />
       <LinkCards />
-      <GetStarted />
+      {/* <GetStarted /> */}
       <Links />
       <Footer />
     </div>
